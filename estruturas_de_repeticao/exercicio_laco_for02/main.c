7328836828c137b0997cebaaa7997a226f472378
eilincoln <115198997+eilincoln@users.noreply.github.com>
2023-09-08 21:43:53 -0300

    estrutura_de_repeticao

---
+++ estruturas_de_repeticao/exercicio_laco_for02/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // decremento no laco for
+
+    int contador;
+
+    for(contador = 100; contador >= 0; contador--) {
+        printf("numero: %d\n", contador);
+    }
+    return 0;
+}
